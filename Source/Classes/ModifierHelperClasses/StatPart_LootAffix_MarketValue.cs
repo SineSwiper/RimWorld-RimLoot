e6b7d29fe12038b98136db5818ecc278d5500a8c
Brendan Byrd <GitHub@ResonatorSoft.org>
2020-04-23 16:30:54 -0400

    Change StatPart_LootAffix_MarketValue formula to account for zero or negative points

--- Source/Classes/ModifierHelperClasses/StatPart_LootAffix_MarketValue.cs
+++ Source/Classes/ModifierHelperClasses/StatPart_LootAffix_MarketValue.cs
@@ -8,26 +8,31 @@ using Verse;
 namespace RimLoot {
     class StatPart_LootAffix_MarketValue : StatPart {
         public override void TransformValue (StatRequest req, ref float val) {
-            if (!req.HasThing) return;
-            if ( !typeof(ThingWithComps).IsAssignableFrom(req.Thing.GetType()) ) return;
-
-            var comp = req.Thing.TryGetComp<CompLootAffixableThing>();
-            if (comp == null) return;
-
-            val *= comp.TotalAffixPoints;
+            float? factor = GetFactor(req);
+            if (factor == null) return;
+            val *= (float)factor;
         }
 
         public override string ExplanationPart (StatRequest req) {
-            if (!req.HasThing) return null;
-            if ( !typeof(ThingWithComps).IsAssignableFrom(req.Thing.GetType()) )  return null;
-
-            var comp = req.Thing.TryGetComp<CompLootAffixableThing>();
-            if (comp == null)  return null;
+            float? factor = GetFactor(req);
+            if (factor == null) return null;
 
             return
                 "RimLoot_LootAffixModifiers".Translate() + ": " +
-                parentStat.Worker.ValueToString(comp.TotalAffixPoints, false, ToStringNumberSense.Factor)
+                parentStat.Worker.ValueToString( (float)factor, false, ToStringNumberSense.Factor )
             ;
+        }
+
+        private float? GetFactor (StatRequest req) {
+            if (!req.HasThing) return null;
+            if ( !typeof(ThingWithComps).IsAssignableFrom(req.Thing.GetType()) ) return null;
+
+            var comp = req.Thing.TryGetComp<CompLootAffixableThing>();
+            if (comp == null) return null;
+
+            double pts = comp.TotalAffixPoints;
+            double exp = Math.Sqrt(Math.Abs(pts)) * Math.Sign(pts);
+            return Math.Pow(2, exp).ChangeType<float>();
         }
     }
 }
